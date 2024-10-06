a9de864a044ce8fbe012dd680d5d1fd5b06b1fc1
florian <florianbridges97@gmail.com>
2024-10-06 12:07:54 +0200

    feat: cast values to float

--- src/gnss_data_analysis.ino
+++ src/gnss_data_analysis.ino
@@ -6,16 +6,16 @@ const char * GNRMC_MESSAGE_HEADER = "$GNRMC,"; // current implementation needs c
 
 // $GNRMC (Minimum GNSS Data)
 struct {
-  char gnss_data_[80];
+  char gnss_data[80];
   bool GetData_Flag;    // Get GNSS data flag bit
   bool ParseData_Flag;  // Parse completed flag bit
   char UTCTime[11];     // UTC time
-  char latitude[11];    // Latitude
+  float latitude;    // Latitude
   char N_S[2];          // N/S
-  char longitude[12];   // Longitude
+  float longitude;   // Longitude
   char E_W[2];          // E/W
-  char speed_over_ground[5];
-  char course_over_ground[7];
+  float speed_over_ground;
+  float course_over_ground;
   bool Usefull_Flag;    // If the position information is valid flag bit
 } parsed_gnss_data;
 
@@ -36,8 +36,8 @@ void setup() {
 }
 
 void loop() {
-  read_raw_gnss_data_(GNRMC_MESSAGE_HEADER);  // Get GNSS data
-  parse_gnss_data_();  // Analyze GNSS data
+  read_raw_gnss_data(GNRMC_MESSAGE_HEADER);  // Get GNSS data
+  parse_gnss_data();  // Analyze GNSS data
   print_gnss_data();  // Output analyzed data
 }
 
@@ -58,7 +58,7 @@ void print_gnss_data() {
     parsed_gnss_data.ParseData_Flag = false;
 
     Serial.println("************************");
-    Serial.println(parsed_gnss_data.gnss_data_);
+    Serial.println(parsed_gnss_data.gnss_data);
 
     
     Serial.print("parsed_gnss_data.UTCTime = ");
@@ -66,24 +66,24 @@ void print_gnss_data() {
     if (parsed_gnss_data.Usefull_Flag) {
       parsed_gnss_data.Usefull_Flag = false;
       Serial.print("parsed_gnss_data.latitude = ");
-      Serial.println(parsed_gnss_data.latitude);
+      Serial.printf("%.5f\n",parsed_gnss_data.latitude);
       Serial.print("parsed_gnss_data.N_S = ");
       Serial.println(parsed_gnss_data.N_S);
       Serial.print("parsed_gnss_data.longitude = ");
-      Serial.println(parsed_gnss_data.longitude);
+      Serial.printf("%.5f\n",parsed_gnss_data.longitude);
       Serial.print("parsed_gnss_data.E_W = ");
       Serial.println(parsed_gnss_data.E_W);
       Serial.print("parsed_gnss_data.speed_over_ground = ");
-      Serial.println(parsed_gnss_data.speed_over_ground);
+      Serial.printf("%.2f\n",parsed_gnss_data.speed_over_ground);
       Serial.print("parsed_gnss_data.course_over_ground = ");
-      Serial.println(parsed_gnss_data.course_over_ground);
+      Serial.printf("%.2f\n",parsed_gnss_data.course_over_ground);
     } else {
       Serial.println("GNSS DATA is not usefull!");
     }
   }
 }
 
-void parse_gnss_data_() {
+void parse_gnss_data() {
   char* subString;
   char* subStringNext;
   if (!parsed_gnss_data.GetData_Flag) {
@@ -94,7 +94,7 @@ void parse_gnss_data_() {
 
   for (int i = 0; i <= 8; i++) {
     if (i == 0) {
-      if ((subString = strstr(parsed_gnss_data.gnss_data_, ",")) == NULL){
+      if ((subString = strstr(parsed_gnss_data.gnss_data, ",")) == NULL){
         error_flag(1);  // Analysis error
       }
       continue;
@@ -115,22 +115,37 @@ void parse_gnss_data_() {
         memcpy(usefullBuffer, subString, subStringNext - subString);
         break;  // Get position status
       case 3:
-        memcpy(parsed_gnss_data.latitude, subString, subStringNext - subString);
+        char lat_buffer_deg[3];
+        char lat_buffer_min[8];
+        memcpy(lat_buffer_deg, subString, 2);
+        memcpy(lat_buffer_min, subString + 2, 7);
+
+        parsed_gnss_data.latitude = atof(lat_buffer_deg) + atof(lat_buffer_min)/60;
         break;  // Get latitude information
       case 4:
         memcpy(parsed_gnss_data.N_S, subString, subStringNext - subString);
         break;  // Get N/S
       case 5:
-        memcpy(parsed_gnss_data.longitude, subString, subStringNext - subString);
+        char lon_buffer_deg[4];
+        char lon_buffer_min[8];
+        memcpy(lon_buffer_deg, subString, 3);
+        memcpy(lon_buffer_min, subString + 3, 7);
+
+        parsed_gnss_data.longitude = atof(lon_buffer_deg) + atof(lon_buffer_min)/60;
         break;  // Get longitude information
       case 6:
         memcpy(parsed_gnss_data.E_W, subString, subStringNext - subString);
         break;  // Get E/W
       case 7:
-        memcpy(parsed_gnss_data.speed_over_ground, subString, subStringNext - subString);
+        char speed_over_ground_buffer[5];
+        memcpy(speed_over_ground_buffer, subString, 4);
+
+        parsed_gnss_data.speed_over_ground = atof(speed_over_ground_buffer);
         break;  // Get speed over ground
       case 8:
-        memcpy(parsed_gnss_data.course_over_ground, subString, subStringNext - subString);
+        char course_over_ground_buffer[7];
+        memcpy(course_over_ground_buffer, subString, 6);
+        parsed_gnss_data.course_over_ground = atof(course_over_ground_buffer);
         break;  // Get course over ground
 
       default:
@@ -146,7 +161,7 @@ void parse_gnss_data_() {
 }
 
 
-void read_raw_gnss_data_(const char * message_header) 
+void read_raw_gnss_data(const char * message_header) 
 {
   while (Serial2.available())
   {
@@ -160,7 +175,7 @@ void read_raw_gnss_data_(const char * message_header)
   if ((gnss_data_head = strstr(rx_buffer, message_header)) == NULL ) return;
   if (((gnss_data_tail = strstr(gnss_data_head, "\r\n")) == NULL) || (gnss_data_tail <= gnss_data_head)) return;
 
-  memcpy(parsed_gnss_data.gnss_data_, gnss_data_head, gnss_data_tail - gnss_data_head);
+  memcpy(parsed_gnss_data.gnss_data, gnss_data_head, gnss_data_tail - gnss_data_head);
   parsed_gnss_data.GetData_Flag = true;
 
   reset_gnss_buffer();
